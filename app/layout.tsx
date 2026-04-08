2a15b7344a19e7a8cedc51cc22a4a9d55df2ced2
leeja <leeja@example.com>
2026-04-08 21:35:56 +0100

    Add Google verification tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+  <html
+    lang="en"
+    className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+  >
+    <head>
+      <meta name="google-site-verification" content="tPkpreMFQXXKQFogA3jpWasjUdq_1nTr_CnmjY_8aNg" />
+    </head>
+
+    <body className="min-h-full flex flex-col">{children}</body>
+  </html>
+);
 }
